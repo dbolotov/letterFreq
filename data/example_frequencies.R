f71bd332d979156b7e24a74b0b31fbe600d32817
dbolotov <dbolotov@live.com>
2014-06-17 14:18:12 -0700

    added oxford freqs

--- data/example_frequencies.R
+++ data/example_frequencies.R
@@ -1,8 +1,12 @@
 #example_frequencies.R
 
-#make 0-count vector
-alphabet <- rep(1,26)
-names(alphabet) <- letters
+#in production, only store data vectors.
+#move processing code out to a separate reproducible script
+
+
+# #make 0-count vector
+# alphabet <- rep(1,26)
+# names(alphabet) <- letters
 
 
 #English classics
@@ -13,3 +17,12 @@ classics_raw <- c(0.0651738,0.0124248,0.0217339,0.0349835,0.1041442,0.0197881,0.
 #recompute freq to exclude space
 classics <- classics_raw[1:26] / sum(classics_raw[1:26])
 names(classics) <- letters
+
+
+#Oxford
+#http://www.oxforddictionaries.com/us/words/what-is-the-frequency-of-the-letters-of-the-alphabet-in-english
+oxford_raw <- c(0.084966,0.020720,0.045388,0.033844,0.111607,0.018121,0.024705,0.030034,0.075448,0.001965,0.011016,0.054893,0.030129,0.066544,0.071635,0.031671,0.001962,0.075809,0.057351,0.069509,0.036308,0.010074,0.012899,0.002902,0.017779,0.002722)
+
+#recompute
+oxford <- oxford_raw / sum(oxford_raw)
+names(oxford) <- letters
